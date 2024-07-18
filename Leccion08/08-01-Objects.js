da0d8169f927ab43780545f99ba9a83a5c4e3398
soy eddy <soyeddy.email.com>
2024-07-17 22:21:04 -0600

    Mas formas de crear un obejto

--- Leccion08/08-01-Objects.js
+++ Leccion08/08-01-Objects.js
@@ -1,8 +1,8 @@
 //Objetos en javaScrip 
 //un obejto puede contener propiedades y metodos
 
-let persona = {
-    nombre:'eddy',
+let persona = {//definir variable que tendra la referencia del obejto que se crea en memoria
+    nombre:'eddy',//propiedad
     apellido:'Perez',
     email:'jperez@gmaol.coom',
     edad : 24,
@@ -17,9 +17,20 @@ console.log(persona.nombre)
 console.log(persona)
 console.log(persona.nombreCompleto())//asi se llama la funcion con el nombre de nuestra propiedad y despues lo parentecis
 
+
 //Mas temas de obejtos en javaScrip
 
+//otra forma de definir un objeto
 
+let persona2 = new Object();//de esta forma se puede crear un obejto nuevo en memoria usando  = palabra reservada (new) Obeject
+/*al usar new Object le estamos indicando a js es que debe crear un nuevo objeto en memoria vacio no contiene ninguna propiedad ya despues
+se le asignan propiedades al obejeto que se van definiendo dinamicamente */
+
+persona2.nombre = 'carlos';//de esta forma se definen las propiedades
+persona2.direccion = 'sector 3';
+persona2.tel = '5885858';
+
+console.log(persona2.nombre)// asi se accede a los obejtos
 
 
 
--- Leccion08/ejercices/objetosEjercices.js
+++ Leccion08/ejercices/objetosEjercices.js
@@ -1,4 +1,4 @@
-//practice --------------------------------------------
+//practice --------------------------------------------Primera Forma
 
 let auto ={
     marca:'toyota',
@@ -24,6 +24,33 @@ let televisor ={
 }
 console.log(televisor.DatosCompletos())
 
-//-------------------------------------------------------------------------
+//-------------------------------------------------------------------------Segunda Forma
+
+let auto2 = new Object()//creando un espacio vacio en memoria (refencia)
+
+auto2.marca = 'porshe'
+auto2.modelo = "52f2fff"
+auto2.color = "negro"
+
+auto2.DatosCompletos = function(){//creamos un metodo
+    return this.marca + ' ' +this.color
+}
+
+console.log(auto2.marca)
+console.log(auto2.color)
+console.log(auto2.DatosCompletos())//llamando la propiedad con el metodo
+
+let televisor2 = new Object()
+
+televisor2.marca = "samsung"
+televisor2.modelo  = "652d5e"
+televisor2.ModeloCompleto = function(){
+    return this.modelo + ' ' + this.marca
+}
+
+console.log(televisor2.ModeloCompleto())
+
+
+//-------------------------------------------------------------------------------
 
 
