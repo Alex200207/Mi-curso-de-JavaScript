8445504c0a0a11ff5e937d72f6f51ef65e5122fc
soy eddy <soyeddy.email.com>
2024-08-12 19:28:54 -0600

    modo strict

---
+++ Leccion10/10-01-ModoStrict.js
@@ -0,0 +1,18 @@
+//Modo Strict
+
+/*Si empezamos a utilizar una varible sin averla declarado javaScript no nos dira nada
+para eso existe el modo strict en javascript para evitar ese tipo de practicas 
+sobretodo cuando trabajamos en proyectos grandes
+ */
+
+//simplemente con agregar la cademna
+"use strict";// debe ser la primera linea de nuestro programa o dentro de una funcion
+//con eso estamos agregando el modo estricto en nuestros programas de javascript
+/*no es necesario agregarlo pero es una buena practica  para encontrar algunos
+errores dentro de nuestro codigo */
+
+x = 10; // x is not defined
+
+
+
+console.log(x);
