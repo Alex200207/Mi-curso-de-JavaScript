bd42a62852750c5263f0500b33bb9e6c90114326
soy eddy <soyeddy.email.com>
2024-08-19 16:22:51 -0600

    ejemplos de promesas y setTimeout

--- Leccion15/15-02-setTimeout&Promesas.js
+++ Leccion15/15-02-setTimeout&Promesas.js
@@ -1,1 +1,26 @@
 //Funcion setTimeout y promesas en javascript
+
+
+let miPromesa = new Promise((resolve,reject)=>{
+    let expresion = true;
+    if(expresion){
+        resolve('se evaluo la expresion correcta mente')
+    }
+    else{
+        reject('la evaluacion ha fallado')
+    }
+})
+
+// miPromesa
+// .then((valor)=> console.log(valor))
+// .catch((err)=> console.log(err))
+
+//llamar la fucion pero utilizando setTimeout asincrono
+
+let promesa = new Promise((resolve)=>{
+    console.log('inicio de la promesa')
+    setTimeout( ()=> resolve ('saludos con promesa y tiemout'),3000)
+    console.log('fin de la promesa')
+});
+
+promesa.then(valor => console.log(valor))
