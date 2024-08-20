c72ffa29938949cc4f239e605c0ffb6c0671f305
soy eddy <soyeddy.email.com>
2024-08-20 17:13:59 -0600

    Manejando el DOM

--- Leccion15/15-05-Async-await-setTimeout.js
+++ Leccion15/15-05-Async-await-setTimeout.js
@@ -18,3 +18,40 @@ async function functionConPromesaAwaitTimeoyt() {//creando funcion asincrona
   // Llamamos a la función 'functionConPromesaAwaitTimeoyt' para ejecutarla.
   functionConPromesaAwaitTimeoyt();
   console.log("Fin de la función");
+
+  //recordar que solo se puede invocar await dentro de una funcion declarada con async
+
+//ejercicio de practica-----------------------------------------------------------------------------------------------
+
+
+// Ejercicio 1: Modificar la función para que espere 5 segundos en lugar de 3 segundos antes de resolver la promesa.
+
+async function ejercicio1(){
+    let miPromesa = new Promise((resolve) =>{
+        setTimeout(() => resolve('promesa cumplida despues de 5 segundos'),5000)
+    })
+    console.log(await miPromesa)
+}
+
+ejercicio1();
+
+// Ejercicio 2: Crear una nueva función asincrónica llamada 'functionConPromesaAwait' que no utilice setTimeout.
+// En su lugar, crea una promesa que se resuelva después de 2 segundos y devuelve el mensaje "promesa con await".
+
+async function functionConPromesaAwait(){
+    let miPromesa = new Promise((resolve) => {
+        resolve('promesa con await')
+    },2000)
+    console.log(await miPromesa)
+}
+functionConPromesaAwait();
+
+
+// Ejercicio 3: Llamar a la función 'functionConPromesaAwait' y mostrar el resultado utilizando console.log.
+
+// Ejercicio 4: Crear una función asincrónica llamada 'functionConPromesaAwaitAll' que utiliza Promise.all para esperar
+// la resolución de múltiples promesas. Crea dos promesas, una que se resuelva después de 1 segundo con el mensaje "promesa 1"
+// y otra que se resuelva después de 3 segundos con el mensaje "promesa 2". Utiliza Promise.all para esperar a que ambas promesas se resuelvan
+// y muestra los resultados utilizando console.log.
+
+// Ejercicio 5: Llamar a la función 'functionConPromesaAwaitAll' y mostrar los resultados utilizando console.log.
---
+++ Leccion16/16-01-DOM-HTML.js
@@ -0,0 +1,12 @@
+//DOM HTML
+
+// DOM HTML
+
+/* Document Object Model (DOM): 
+   Es una interfaz de programación para documentos HTML y XML. 
+   Representa la estructura del documento como un árbol de nodos, 
+   donde cada nodo es un objeto que representa una parte del documento. 
+   Esto permite a los lenguajes de programación manipular la estructura, 
+   estilo y contenido del documento de manera dinámica.
+*/
+//para js cada elemento del dom es un objeto asi que podremos manipularlo
